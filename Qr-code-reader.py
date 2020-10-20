896ef1235d47562c668d746043933e2d1fc1f401
MsDxGB <48661966+MsDxGB@users.noreply.github.com>
2020-10-20 18:03:32 +0100

    Update Qr-code-reader.py

--- Qr-code-reader.py
+++ Qr-code-reader.py
@@ -7,37 +7,37 @@ from pyzbar import pyzbar
 
 #############################
 
-
+# First Function is Decoding the information from the (barcode , Qr Code )  , drawing a rectangle around it ,show the decoded information , and exporting the information into a text document
 def read_barcodes(frame):
     barcodes = pyzbar.decode(frame)
     for barcode in barcodes:
         x, y , w, h = barcode.rect
-        #1
         barcode_info = barcode.data.decode('utf-8')
+        
         cv2.rectangle(frame, (x, y),(x+w, y+h), (0, 255, 0), 2)
         
-        #2
         font = cv2.FONT_HERSHEY_DUPLEX
         cv2.putText(frame, barcode_info, (x + 6, y - 6), font, 2.0, (255, 255, 255), 1)
-        #3
-        with open("barcode_result.txt", mode ='w') as file:
+        
+        with open("bar&QRcode_result.txt", mode ='w') as file:
             file.write("Recognized Barcode:" + barcode_info)
     return frame
 
+# Turn on the video camera of the computer, and the then call the decoding function
 def main():
-    #1
+    
     camera = cv2.VideoCapture(0)
     ret, frame = camera.read()
-    #2
+    
     while ret:
         ret, frame = camera.read()
         frame = read_barcodes(frame)
         cv2.imshow('Barcode/QR code reader', frame)
         if cv2.waitKey(1) & 0xFF == 27:
             break
-    #3
+    
     camera.release()
     cv2.destroyAllWindows()
-#4
+
 if __name__ == '__main__':
     main()
